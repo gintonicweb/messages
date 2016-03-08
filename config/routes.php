9af4a02a6500c8b6dcd004e5144de54ee39ee434
Philippe Lafrance <phil.laf@gmail.com>
2016-03-08 14:34:57 -0500

    Fallback routes should be handled by the app and not plugins.

--- config/routes.php
+++ config/routes.php
@@ -3,14 +3,11 @@ use Cake\Routing\Router;
 
 Router::scope('/', ['plugin' => 'Messages'], function ($routes) {
 
-    $routes->fallbacks('DashedRoute');
-
     $routes->prefix('api', function ($routes) {
         $routes->extensions(['json','xml']);
         $routes->resources('Messages.Threads');
         $routes->resources('Messages.Messages');
         $routes->connect('/messages/:action/*', ['controller' => 'Messages']);
         $routes->connect('/threads/:action/*', ['controller' => 'Threads']);
-        $routes->fallbacks('DashedRoute');
     });
 });
